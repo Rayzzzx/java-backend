100898e6c97fc1fb225af1f4110a4b5f21a23d8c
Rayzzzx <rayzzzx13@gmail.com>
2024-10-28 16:27:20 -0500

    add /greet endpoint

--- src/SimpleHttpServer.java
+++ src/SimpleHttpServer.java
@@ -9,9 +9,12 @@ import java.util.logging.Logger;
 import java.util.logging.Level;
 
 public class SimpleHttpServer {
+    private static final Logger logger = Logger.getLogger(SimpleHttpServer.class.getName());
+
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/hello", new HelloHandler());
+        server.createContext("/greet", new GreetHandler());
         server.setExecutor(null); // create a default executor
         server.start();
         System.out.println("Server started on port 8080");
@@ -26,5 +29,26 @@ public class SimpleHttpServer {
             os.write(response.getBytes());
             os.close();
         }
+    }
+
+    static class GreetHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            logRequest(exchange);
+            String query = exchange.getRequestURI().getQuery();
+            String name = "World";
+            if (query != null && query.startsWith("name=")) {
+                name = query.split("=")[1];
+            }
+            String response = "Hello, " + name + "!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    private static void logRequest(HttpExchange exchange) {
+        logger.info("Received request: " + exchange.getRequestMethod() + " " + exchange.getRequestURI());
     }
 }
